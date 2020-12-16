e53fef40fe43a9e4846aa5be074babae6f7476ad
ishige-shogo <shigeshigeishige@gmail.com>
2020-12-16 09:58:52 +0000

    [add]3routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
   devise_for :customers
   devise_for :admins
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :admin do                                                  #admin/customersコントローラのルーティングです　5-7行
+    resources :customers, only:[:index, :edit, :update, :show]
+  end
+
+  scope module: :customer do                                          #customer/receiversコントローラのルーティングです　9-11行
+    resources :receivers, except:[:new, :show]
+  end
+
+  scope module: :customer do                                          #customer/customersコントローラのルーティングです  13-18行
+    resource :customers, only:[:show, :edit, :update]                 #showとeditのURLに:idを含ませないためresourceを単数形にしています
+  end
+
+  get "/customers/unsubscribe" => "customer/customers#unsubscribe"    #getメソッドでunsubscribeアクションにつながります
+  patch "/customers" => "customer/customers#withdraw"                 #patchメソッドでwithdrawアクションにつながります
+
 end
