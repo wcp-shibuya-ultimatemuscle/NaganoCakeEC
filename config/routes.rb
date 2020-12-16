c0a9e4ac9f8d06848c6b350c26637bc8fef8a16c
KKe-coder <carumag726@gmail.com>
2020-12-16 08:46:51 +0000

    [Add]deviseのルーティング追加、URLの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  devise_for :admins
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :customers, path: 'customer', controllers: {
+  sessions:      'customer/sessions',
+  passwords:     'customer/passwords',
+  registrations: 'customer/registrations'
+  }
+
+  devise_for :admins, path: 'admin', controllers: {
+  sessions:      'admin/sessions',
+  passwords:     'admin/passwords',
+  registrations: 'admin/registrations'
+  }
+
 end
