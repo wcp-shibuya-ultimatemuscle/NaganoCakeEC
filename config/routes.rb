823edf716177e7630ca5f59b0214e09b2ede7fc7
yusukeishino <yskgori0407@gmail.com>
2020-12-16 11:05:52 +0000

    order,jenre,productルート設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,19 @@ Rails.application.routes.draw do
   devise_for :customers
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+   resources :orders, only: [:index, :show, :update]
+  end
+
+  get 'admin/orders/top' => 'admins#top'
+
+  namespace :admin do
+   resources :genres, only: [:index, :update, :edit, :create]
+  end
+
+  namespace :admin do
+   resources :product, only: [:index, :new, :create, :show, :edit, :update]
+  end
+
 end
+
