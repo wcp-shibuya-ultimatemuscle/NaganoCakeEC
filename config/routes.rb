1e5fc7e17fd4e0db9e42656e3183c3c84e88dbae
yusukeishino <yskgori0407@gmail.com>
2020-12-16 12:27:43 +0000

    admin/oders#top修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,20 @@ Rails.application.routes.draw do
   devise_for :customers
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #admins/orderのルーティング
   namespace :admin do
    resources :orders, only: [:index, :show, :update]
   end
 
-  get 'admin/orders/top' => 'admins#top'
+  get 'admin/order/top' => 'admins/orders#top'
 
+  #admins/genreのルーティング
   namespace :admin do
    resources :genres, only: [:index, :update, :edit, :create]
   end
 
+  #admins/productのルーティング
   namespace :admin do
    resources :product, only: [:index, :new, :create, :show, :edit, :update]
   end
