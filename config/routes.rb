28778b91ed461f979a93034e563a8cfa1d935154
KKe-coder <carumag726@gmail.com>
2020-12-16 08:08:20 +0000

    [Add]rootパス/aboutパスに対するroutes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :customers
   devise_for :admins
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'customer/homes#top'
+  get '/about', to: 'customer/homes#about'
+
 end
