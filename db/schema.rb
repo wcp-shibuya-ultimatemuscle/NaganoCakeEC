366c863abd288fd71d8d141e7f76df86279fbf17
KKe-coder <carumag726@gmail.com>
2020-12-16 03:25:06 +0000

    [Update] Customerモデルの追加,devise.rbの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_16_024047) do
+ActiveRecord::Schema.define(version: 2020_12_16_025804) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,26 @@ ActiveRecord::Schema.define(version: 2020_12_16_024047) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "family_name"
+    t.string "first_name"
+    t.string "family_kana"
+    t.string "first_kana"
+    t.string "postal_code"
+    t.string "address"
+    t.string "tel"
+    t.boolean "is_deleted"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_customers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
 end
