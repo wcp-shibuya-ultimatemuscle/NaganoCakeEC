2c7b4478fb3f79a6ea549fd5443012025f1f5a3c
ishige-shogo <shigeshigeishige@gmail.com>
2020-12-19 07:12:50 +0000

    update_receiver

--- app/controllers/customer/receivers_controller.rb
+++ app/controllers/customer/receivers_controller.rb
@@ -1,2 +1,21 @@
 class Customer::ReceiversController < ApplicationController
+
+  def index
+    @receiver = Receiver.new
+    @receivers = Receiver.where(customer_id: current_customer.id)
+  end
+
+  def create
+    @receiver = Receiver.new(receiver_params)
+    @receiver.customer_id = current_customer.id
+    @receiver.save
+    redirect_to receivers_path
+  end
+
+
+  private
+
+  def receiver_params
+    params.require(:receiver).permit(:postal_code, :address, :name)
+  end
 end
