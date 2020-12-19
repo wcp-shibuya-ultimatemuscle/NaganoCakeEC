1eb89293f64b9e6536c93f92b2d326a4ce86107a
yusukeishino <yskgori0407@gmail.com>
2020-12-19 06:23:41 +0000

    商品一覧と商品詳細の枠

--- app/controllers/customer/products_controller.rb
+++ app/controllers/customer/products_controller.rb
@@ -1,2 +1,19 @@
 class Customer::ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    #@product.save
+    #redirect_to cart_items_path
+  end
+
+  private
+
+  def product_params
+    params.require(@product).permit(:name, :tax_out_price)
+  end
+
 end
