0621c503df2ec520fb77bf26934a542764d642ea
zunema <yxxa.abc@gmail.com>
2020-12-20 06:43:20 +0000

    page20ショッピングカートの作成

--- app/controllers/customer/cart_items_controller.rb
+++ app/controllers/customer/cart_items_controller.rb
@@ -1,2 +1,16 @@
 class Customer::CartItemsController < ApplicationController
+  
+  def index
+    @products = Product.all
+  end 
+  
+  def destroy
+    
+  end 
+  
+  def destroy_all
+    CartItems.destroy_all
+    redirect_to cart_items_path（）
+end
+  
 end
