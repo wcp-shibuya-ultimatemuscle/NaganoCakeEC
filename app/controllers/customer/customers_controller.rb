d2d6f3f8b57a1f471588f10a30bb6a863dfa7c7b
ishige-shogo <shigeshigeishige@gmail.com>
2020-12-20 04:02:52 +0000

    mypagedestroy

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -14,10 +14,23 @@ class Customer::CustomersController < ApplicationController
     redirect_to customer_path(current_customer.id)
   end
 
+  def unsubscribe
+  end
+
+  def withdraw
+    customer = Customer.find(current_customer.id)
+    customer.is_deleted = true
+    customer.update(customer_is_deleted)
+    redirect_to root_path
+  end
+
 
   private
   def customer_params
      params.require(:customer).permit(:email, :family_name, :first_name, :family_kana, :first_kana, :postal_code, :address, :tel, :is_deleted)
+  end
 
+  def customer_is_deleted
+     params.permit(:is_deleted)
   end
 end
