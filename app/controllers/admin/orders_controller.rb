16b2f9e75c28e1555b285125399f7112f2a061d5
zunema <yxxa.abc@gmail.com>
2020-12-20 04:13:14 +0000

    page13（管理者）注文履歴一覧画面の作成

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,5 +3,9 @@ class Admin::OrdersController < ApplicationController
   def top
     @todayorder = Order.where(created_at: Time.now.all_day) #本日作成したOrderを代入
   end
+  
+  def index
+    @orders = Order.all
+  end 
 
 end
