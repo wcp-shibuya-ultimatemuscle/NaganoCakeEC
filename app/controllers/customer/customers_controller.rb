7e4cbec82da5c904067b4d9c3691961ebc2310e3
ishige-shogo <shigeshigeishige@gmail.com>
2020-12-20 01:19:04 +0000

    mypageedit

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -1,9 +1,23 @@
 class Customer::CustomersController < ApplicationController
 
   def show
-    @customer = Customer.find(current_customer.id)   #のちに @customer = Customer.find(current_customer.id) になります
+    @customer = Customer.find(current_customer.id)
   end
 
   def edit
+    @customer = Customer.find(current_customer.id)
+  end
+
+  def update
+    customer = Customer.find(current_customer.id)
+    customer.update(customer_params)
+    redirect_to customer_path(current_customer.id)
+  end
+
+
+  private
+  def customer_params
+     params.require(:customer).permit(:email, :family_name, :first_name, :family_kana, :first_kana, :postal_code, :address, :tel, :is_deleted)
+
   end
 end
