20bc06f8b7d2869c204e3c91a385428bb9cc8771
K^2e <72348241+KKe-coder@users.noreply.github.com>
2020-12-21 10:10:39 +0900

    homese_controller修正

--- app/controllers/customer/homes_controller.rb
+++ app/controllers/customer/homes_controller.rb
@@ -1,7 +1,7 @@
 class Customer::HomesController < ApplicationController
 
   def top
-    @genres = Genre.all
+    @genres = Genre.where(is_active: "有効")
     @products = Product.where(genre_id: Genre.where(is_active: "有効")).last(4)
   end
 
