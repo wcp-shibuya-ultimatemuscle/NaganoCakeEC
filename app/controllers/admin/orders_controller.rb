d1b8499261359a9f3ffceb89729644eda18f98d5
KKe-coder <carumag726@gmail.com>
2020-12-22 04:10:20 +0000

    ⑫(管理者)注文履歴詳細 注文部分

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,18 +3,27 @@ class Admin::OrdersController < ApplicationController
   def top
     @todayorder = Order.where(created_at: Time.now.all_day) #本日作成したOrderを代入
   end
-  
-  def index
-    @orders = Order.all
-  end 
 
   def index
+    @orders = Order.all
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    @order.status = params[:status]
+    @order.update(orders_params)
+    redirect_to request.referer
   end
 
   def show
-    #@customer = Customer.find(params[:id])
-    #@order = Order.find(params[:id])
+    @order = Order.find(params[:id])
     @order_products = OrderProduct.all
+  end
+
+  private
+
+  def orders_params
+    params.require(:order).permit(:customer_id, :postal_code, :status, :address, :name, :shipping, :payment, :sum)
   end
 
 end
---
+++ db/migrate/20201221134450_change_payment_of_orders.rb
@@ -0,0 +1,9 @@
+class ChangePaymentOfOrders < ActiveRecord::Migration[5.2]
+  def up
+    change_column :orders, :payment, :integer
+  end
+
+  def down
+    change_column :orders, :payment, :integer, default: '0'
+  end
+end
