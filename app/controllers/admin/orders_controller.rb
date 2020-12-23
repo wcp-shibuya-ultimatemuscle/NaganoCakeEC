b730e183ed20db8848e3d824146a00324ddcafec
KKe-coder <carumag726@gmail.com>
2020-12-23 15:23:42 +0000

    [Fix]admin/topアクション

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,7 @@
 class Admin::OrdersController < ApplicationController
 
   def top
-    @todayorder = Order.where(created_at: Time.now.all_day) #本日作成したOrderを代入
+    @todayorder = Order.where(created_at: Time.current.all_day) #本日作成したOrderを代入
   end
 
   def index
