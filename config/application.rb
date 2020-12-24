a8ab31d9591e978b08afc019e1ef30ff04393399
KKe-coder <carumag726@gmail.com>
2020-12-24 10:10:43 +0000

    [Update]エラーメッセージの分割表示(devise)のまとめ、但しapplication.rb application_helper.rbをいじっている為マージ禁止　他の部分にも適用される

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module Naganocake
       else
         class_name = instance.object.class.name.underscore
         method_name = instance.instance_variable_get(:@method_name)
-        "<label class=\"huga\">#{html_tag}</label><br>
+        "<label class=\"formcolor\">#{html_tag}</label><br>
           <span class=\"error_field alert-danger errormsg\">
             #{I18n.t("activerecord.attributes.#{class_name}.#{method_name}")}
             #{instance.error_message.first}
