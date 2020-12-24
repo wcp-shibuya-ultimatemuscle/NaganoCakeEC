55ead8ddf08c58650df1feacc81886572d618d69
yusukeishino <yskgori0407@gmail.com>
2020-12-24 13:42:33 +0000

    画像プレビュー追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,19 +19,3 @@
 //= require_tree .
 
 
-<script>
-$(document).on("turbolinks:load", function(){
-  function readURL(input) {
-    if(input.files && input.files[0]){
-      var reader = new FileReader();
-      reader.onload = function (e) {
-        $('#img_prev').attr('src', e.target.result);
-      }
-      reader.readAsDataURL(input.files[0]);
-    }
-  }
-  $('.image').change(function(){
-    readURL(this);
-  });
-});
-</script>
