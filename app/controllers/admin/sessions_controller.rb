5d77f414b01e920454986e8653685223435b5dab
KKe-coder <carumag726@gmail.com>
2020-12-26 00:58:52 +0000

    bugfix

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -30,4 +30,9 @@ class Admin::SessionsController < Devise::SessionsController
   def after_sign_in_path_for(resource)
     admin_orders_top_path
   end
+
+  def after_sign_out_path_for(resource)
+    new_admin_session_path
+  end
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -10,7 +10,7 @@ class Order < ApplicationRecord
   # 支払方法
   enum payment: {銀行振込: 0, クレジットカード: 1}
   # 注文ステータス
-  enum status: {入金待ち: 0, 入金確認: 1, 製作中: 2, 発送準備中: 3, 発送: 4}
+  enum status: {入金待ち: 0, 入金確認: 1, 製作中: 2, 発送準備中: 3, 発送済み: 4}
 
 
 end
